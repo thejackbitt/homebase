9ec5e14f59bf78c93ac35bccb4f7dfc10cb4af8e
Jack Bittner <71474581+thejackbitt@users.noreply.github.com>
2023-12-12 18:48:31 -0600

    added two new pages and full email functionality

--- assets/script.js
+++ assets/script.js
@@ -37,3 +37,127 @@ document.addEventListener('DOMContentLoaded', (event) => {
         topBar.style.backgroundColor = newColor;
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var bookImages = document.querySelectorAll('.book-box img');
+    bookImages.forEach(function(img) {
+        img.addEventListener('click', function() {
+            // Add the 'shrink' class to shrink the element
+            img.classList.add('shrink');
+
+            // Set a timeout to remove the class, returning it to its original size
+            setTimeout(function() {
+                img.classList.remove('shrink');
+            }, 200); // 500 milliseconds delay
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.doc-button').forEach(button => {
+      button.addEventListener('click', () => {
+          const docViewer = document.querySelector('.doc-viewer');
+
+          if (docViewer) {
+              docViewer.scrollIntoView({ behavior: 'smooth', block: 'end' });
+          }
+      });
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button and the element to change
+    var button = document.getElementById('CH1-button');
+    // Select the first element with the class 'doc-viewer'
+    var elementToChange = document.getElementsByClassName('doc-viewer')[0];
+
+    // Add click event listener to the button
+    button.addEventListener('click', function() {
+        // Change the HTML content of the element
+        elementToChange.innerHTML = `
+            <div class="doc-viewer" id="CH1-viewer">
+              <object data="/assets/pdf/RAM_Issue1.pdf" type="application/pdf">
+                <p>Alternative text - include a link <a href="/assets/pdf/RAM_Issue1.pdf">to the PDF!</a></p>
+              </object>
+            </div>`;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button and the element to change
+    var button = document.getElementById('CH2-button');
+    // Select the first element with the class 'doc-viewer'
+    var elementToChange = document.getElementsByClassName('doc-viewer')[0];
+
+    // Add click event listener to the button
+    button.addEventListener('click', function() {
+        // Change the HTML content of the element
+        elementToChange.innerHTML = `
+            <div class="doc-viewer" id="CH2-viewer">
+              <object data="/assets/pdf/RAM_Issue2.pdf" type="application/pdf">
+                <p>Alternative text - include a link <a href="/assets/pdf/RAM_Issue2.pdf">to the PDF!</a></p>
+              </object>
+            </div>`;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button and the element to change
+    var button = document.getElementById('AI-button');
+    // Select the first element with the class 'doc-viewer'
+    var elementToChange = document.getElementsByClassName('doc-viewer')[0];
+
+    // Add click event listener to the button
+    button.addEventListener('click', function() {
+        // Change the HTML content of the element
+        elementToChange.innerHTML = `
+        <div class="doc-viewer" id="AI-viewer">
+        <div class="doc-viewer-backdrop">
+          
+          <img class="animate-in-1" src="/assets/images/Room3Artwork.jpg">
+          <div class="header-container animate-in-1">
+            <p>Each chapter goes through these concepts in detail and provides practical applications for business purposes. This business is perfect for small businesses, entrepreneurs and individuals looking to break into the tech industry and get on the ground floor of something big. Jack is passionate about democratizing technical skills and wants to put the power of big tech back in the hands of the everyday American.</p>
+            <a href="https://www.amazon.com/Generative-Business-Purposes-Principles-Diffusion-ebook/dp/B0CLPN3Q91/ref=tmm_kin_swatch_0?_encoding=UTF8&qid=1699912515&sr=8-1" target="_blank">
+              <button class="custom-btn btn-main">
+                  View on Amazon
+              </button>
+            </a>
+          </div>
+        </div>
+          <div class="effects">
+            <div class="laser-overlay"></div>
+            <div class="laser"></div>
+            <div class="glowbox"></div>
+          </div>
+      </div>`;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('submitBtn').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Extract the form data
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Define the email parameters
+    var emailParams = {
+      from_email: email,
+      message_html: message
+    };
+
+    // Send the email
+    emailjs.send('service_4c5bfis', 'template_pw8m169', emailParams) // Pass emailParams here
+      .then(function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+        window.location.href = '/thankyou.html'; // Redirect to thankyou.html
+      }, function (error) {
+        console.log('FAILED...', error);
+        alert('Failed to send the email. Please try again.');
+      });
+  });
+});
+
+
